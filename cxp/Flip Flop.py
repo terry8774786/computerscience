e33f0a59670ff28f048aaaf1df76c62c895f937c
Terry835516 <835516@mystma.org>
2024-11-15 14:41:44 -0600

    update

---
+++ cxp/Flip Flop.py
@@ -0,0 +1,19 @@
+from adafruit_circuitplayground import cp
+
+import time
+
+while True:
+    if cp.switch
+    for i in range(0,5)
+    cp.pixels[i] = (0, 15, 0)
+else:
+    for i in range(5,10):
+        cp.pixels[i] = (0, 10, 0)
+
+    if cp.switch:
+        for j in range(5,10):
+            cp.pixels[j] = (0, 0, 0)
+        else:
+            for j in range(0,5):
+                cp.plxels[j] = (0, 0, 0)
+                
